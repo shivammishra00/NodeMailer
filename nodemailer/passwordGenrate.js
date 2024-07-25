8f3f9d7f1f7befdc03495defb8ef69f3ddad1a86
shivammishra00 <shivam887823950@gmail.com>
2024-07-25 16:38:51 +0530

    final commit

--- nodemailer/passwordGenrate.js
+++ nodemailer/passwordGenrate.js
@@ -1,4 +1,6 @@
 /////======   automatic password genrate kar sakte hi   ======//
+////  ye java script ki functionality hai ise hum frontend side use kar sakte hai //
+
 const  generator = require('generate-password');
 // console.log(generator)
 
