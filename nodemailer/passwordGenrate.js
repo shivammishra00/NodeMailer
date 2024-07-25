daae6ec02d4e5047e5927274cfa8095e6ae9ac5f
shivammishra00 <shivam887823950@gmail.com>
2024-07-25 16:20:08 +0530

    some changes

--- nodemailer/passwordGenrate.js
+++ nodemailer/passwordGenrate.js
@@ -1,7 +1,8 @@
+/////======   automatic password genrate kar sakte hi   ======//
 const  generator = require('generate-password');
 // console.log(generator)
 
-const password = generator.generate({
+const password = generator.generate({   /// genrate method hai jo object accept karti hai 
     length: 20,
     numbers: true,
     symbols: true,
